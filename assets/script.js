d747923639b73f25026c413247d70c1314ff5763
mjlindskog <lindskogmarkj@gmail.com>
2021-06-08 18:53:50 -0400

    updated html so all elements are present + added moment

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,32 @@
 var searchBtn = $("#searchBtn");
-var searchedCity = $("#searchBar");
-var currentDate = moment().format("LLLL");
-var forecastDate = moment().format("")
+var searchedCity = $("#searchBar").val();
+var date = moment();
+var currentDate = date.format("LLLL");
+var forecastOne = date.add(1, "day").format("l");
+var forecastTwo = date.add(2, "days").format("l");
+var forecastThree = date.add(3, "days").format("l");
+var forecastFour = date.add(4, "days").format("l");
+var forecastFive = date.add(5, "days").format("l");
+
+var apiKey = "7211e2dd4d2c7c0bb475207d2efebac6";
+var currentWeather = "api.openweathermap.org/data/2.5/weather?q=" + searchedCity + "&appid=" + apiKey + "&units=imperial"
+var forecastWeather = "api.openweathermap.org/data/2.5/forecast?q=" + searchedCity + "&appid=" + apiKey + "&units=imperial"
+
+$(document).ready(function() {
+    $(".currentDate").text(currentDate);
+
+    $(".weatherOne").text(forecastOne);
+    $(".weatherTwo").text(forecastTwo);
+    $(".weatherThree").text(forecastThree);
+    $(".weatherFour").text(forecastFour);
+    $(".weatherFive").text(forecastFive);
+})
+
+searchBtn.click(function() {
+    $.ajax({
+        url:currentWeather,
+        method:"GET",
+    }).then(function (response){
+
+    });
+})
